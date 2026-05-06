2f120c56f52e57c4f69dd4b3036ea8a51cd951f9
deepakvalmani <rajvalmani@gmail.com>
2026-05-06 23:31:05 +0500

    Remove all files from the repository

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, verifyOTP } = require('../controllers/authController');
-
-router.post('/register', register);
-router.post('/verify-otp', verifyOTP);
-
-module.exports = router;
